369e6dc6dd5c3252a0bc9569d848d987124c4d6a
Alberinea <kan9870@gmail.com>
2021-05-31 03:00:23 +0900

    Changed double quotes to single quotes for readability and fixed some texts

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
 const computerAction = Math.floor(Math.random() * 3);
 const computerPlay =
-  computerAction === 0 ? "rock" : computerAction === 1 ? "paper" : "scissors";
+    computerAction === 0 ? 'rock' : computerAction === 1 ? 'paper' : 'scissors';
 
 const action = {
-  rock: { losesTo: "paper", winsAgainst: "scissors" },
-  paper: { losesTo: "scissors", winsAgainst: "rock" },
-  scissors: { losesTo: "rock", winsAgainst: "paper" },
+    rock: { losesTo: 'paper', winsAgainst: 'scissors' },
+    paper: { losesTo: 'scissors', winsAgainst: 'rock' },
+    scissors: { losesTo: 'rock', winsAgainst: 'paper' },
 };
 
 function playRound(playerSelection, computerSelection) {
-  let playerAction = prompt("Type 'Rock, Paper or Scissors'");
-  playerSelection = playerAction.toLowerCase();
-  computerSelection = computerPlay;
+    let playerAction = prompt('Type "Rock", "Paper" or "Scissors"');
+    playerSelection = playerAction.toLowerCase();
+    computerSelection = computerPlay;
 
-  return action[playerSelection]?.winsAgainst === computerSelection
-    ? alert(
-        `You win! ${playerSelection[0].toUpperCase()}${playerSelection.substring(
-          1
-        )} beats ${computerSelection[0].toUpperCase()}${computerSelection.substring(
-          1
-        )}`
-      )
-    : action[playerSelection]?.losesTo === computerSelection
-    ? alert(
-        `You lose! ${computerSelection[0].toUpperCase()}${computerSelection.substring(
-          1
-        )} beats ${playerSelection[0].toUpperCase()}${playerSelection.substring(
-          1
-        )}`
-      )
-    : playerSelection === computerSelection 
-    ? alert("Draw!")
-    : alert("Wrong inputs, try again");
+    return action[playerSelection]?.winsAgainst === computerSelection
+        ? alert(
+              `You win! ${playerSelection[0].toUpperCase()}${playerSelection.substring(
+                  1
+              )} beats ${computerSelection[0].toUpperCase()}${computerSelection.substring(
+                  1
+              )}`
+          )
+        : action[playerSelection]?.losesTo === computerSelection
+        ? alert(
+              `You lose! ${computerSelection[0].toUpperCase()}${computerSelection.substring(
+                  1
+              )} beats ${playerSelection[0].toUpperCase()}${playerSelection.substring(
+                  1
+              )}`
+          )
+        : playerSelection === computerSelection
+        ? alert('Draw!')
+        : alert('Wrong inputs, try again');
 }
 
 playRound();
